37507b064530cb564348953275666334938f6f04
ChitraNaresh <129093352+ChitraNaresh@users.noreply.github.com>
2023-10-19 09:38:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+// const dbConfig=require("./config/dbConfig")
+const dbConfig=require("./dbConfig")
+const app = express();
+require("dotenv").config()
+app.use(express.json())
+// const userRoute=require("./routes/userRoute")
+const userRoute=require("./userRoute")
+app.use("/api/user",userRoute)
+app.use("/api/admin",userRoute)
+app.use("/api/doctor",userRoute)
+const port = process.env.PORT || 5001;
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
